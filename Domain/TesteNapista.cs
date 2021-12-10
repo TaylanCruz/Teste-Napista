fd06baca8ffdec6f6ba244e89276d036cc2c315e
TaylanCruz <tnccelestino@dcomp.ufs.br>
2021-12-10 01:27:52 -0300

    Adicionando domain

---
+++ Domain/TesteNapista.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteNapistaApi.Domain
+{
+    public sealed class TesteNapista
+    {
+        public long Id { get; set; }
+        public String nome { get; set; }
+        public int valor_unitario { get; set; }
+        public String qtde_estoque { get; set; }
+    }
+}
